821cd76aa701f204dc73f9f453f2507cda93bac7
Kanwal Fatima <hafizakanwal0310222@gmail.com>
2024-11-22 09:44:49 -0700

    Add files via upload

---
+++ slider/slider.js
@@ -0,0 +1,18 @@
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    currentIndex += direction;
+
+    
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    slider.style.transform = `translateX(-${currentIndex * 1000}px)`; 
+}
